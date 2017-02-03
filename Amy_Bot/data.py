612134214d7b6c80c6dcd21b76674ac58c3ac7a2
Tyler Kelly <tkelly@cs.fsu.edu>
2017-02-03 12:34:33 -0500

    data.py

---
+++ Amy_Bot/data.py
@@ -0,0 +1,6 @@
+banned_subs = ['audiophile', 'IAmA', 'MechanicalKeyboards', 'CCW', 'SuggestALaptop']
+user_agent='Amy v0.1'
+client_id='ZPTLrdpIbtlGoA'
+client_secret='JbjkkWH0YzlgGwXyVDxAs2SAT7Y'
+username='AMY_bot',
+password='encryptedpasswordbot'
