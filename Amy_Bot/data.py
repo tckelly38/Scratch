03d5175b5b589db8676ec82a14bf1f1f04318df8
Tyler Kelly <tkelly@cs.fsu.edu>
2017-02-04 16:13:34 -0500

    Update data.py

--- Amy_Bot/data.py
+++ Amy_Bot/data.py
@@ -1,6 +1,6 @@
 banned_subs = ['audiophile', 'IAmA', 'MechanicalKeyboards', 'CCW', 'SuggestALaptop']
 user_agent='Amy v0.1'
 client_id='ZPTLrdpIbtlGoA'
-client_secret='JbjkkWH0YzlgGwXyVDxAs2SAT7Y'
+client_secret=''
 username='AMY_bot',
-password='encryptedpasswordbot'
+password=''
